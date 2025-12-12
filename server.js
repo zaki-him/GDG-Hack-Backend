1daa15fcc1628318cb61ccbc04a0ad00b9c48f2c
zakaria <himranezakaria2@gmail.com>
2025-12-12 23:01:36 +0100

    .

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import router from './src/routes/router.js'
 import dotenv from "dotenv"
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/v1', router)
 
